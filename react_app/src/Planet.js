eebbc20cb216bad5e2a084ba1c90fd3f7afa5864
leonardo <leogoca1300@gmail.com>
2024-10-06 18:03:22 -0500

    se agrego una imagen y se creo un array de imagenes de plaentas

--- react_app/src/Planet.js
+++ react_app/src/Planet.js
@@ -1,6 +1,8 @@
 import { useEffect, useState, useRef } from "react";
 import { DrawingCanvas } from "./DrawingCanvas";
 
+const planetImages= [];
+
 export function SpaceView(props) {
   return (
     <div className="w-full h-screen relative">
@@ -19,28 +21,10 @@ export function SpaceView(props) {
 }
 
 export function Planet(props) {
-  const [rotation, setRotation] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setRotation((prev) => (prev + 1) % 360);
-    }, 50);
-
-    return () => clearInterval(interval);
-  }, []);
 
   return (
     <div className="relative perspective-[1000px]">
-      <div
-        className="size-96 rounded-full transition-transform duration-300 ease-linear"
-        style={{
-          backgroundColor: "#fff",
-          transform: `rotateY(${rotation}deg)`,
-          backgroundImage:
-            "linear-gradient(45deg, rgba(0,0,0,0.2) 0%, transparent 50%, rgba(255,255,255,0.2) 100%)",
-        }}
-        onClick={() => props.setShowStarView(true)}
-      />
+      <img src="/planeta1.jpg" className="size-80 rounded-full"/>
     </div>
   );
 }
